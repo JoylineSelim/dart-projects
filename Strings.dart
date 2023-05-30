fe6c25cd50ade1cb3ea128650401d89fd24c7740
Joyline <134996568+JoylineSelim@users.noreply.github.com>
2023-05-30 04:17:40 -0700

    Create Strings.dart

---
+++ Strings.dart
@@ -0,0 +1,10 @@
+void main(){
+String character ="Oscar";
+  String age="20";
+
+  print("There was a man named ${character}");
+  print("He was ${age}");
+  character="John";
+  print("He loved the name ${character}");
+  print("But he did not love being ${age}");
+}
